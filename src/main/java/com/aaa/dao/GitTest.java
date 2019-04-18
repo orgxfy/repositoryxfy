d5a421e43f193a467f06313ac3224f0bb2e2e9d4
xfy <1574684396@qq.com>
2019-04-18 20:00:08 +0800

    第二次提交

--- src/main/java/com/aaa/dao/GitTest.java
+++ src/main/java/com/aaa/dao/GitTest.java
@@ -8,6 +8,6 @@ package com.aaa.dao;
  */
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello！");
+        System.out.println("hello1！");
     }
 }
