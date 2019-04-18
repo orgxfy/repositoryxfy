0e48540b6d40c4b6625065c08cbe1c33d76fa94f
xfy <1574684396@qq.com>
2019-04-18 19:40:40 +0800

    第一次提交

---
+++ src/main/java/com/aaa/dao/GitTest.java
@@ -0,0 +1,13 @@
+package com.aaa.dao;
+
+/**
+ * fileName:GitTest
+ * description:
+ * author:xfy
+ * createTime:2019/4/18 19:38
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello！");
+    }
+}
